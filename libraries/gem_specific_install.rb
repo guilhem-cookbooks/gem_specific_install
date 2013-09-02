f44d479cedef597b20a6c4a15d8aa13558b86a99
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-09-02 15:02:05 +0200

    Last work who "must" work

--- libraries/gem_specific_install.rb
+++ libraries/gem_specific_install.rb
@@ -2,30 +2,16 @@
 require 'chef/provider/package/rubygems'
 class Chef::Provider::Package::Rubygems::SpecificInstall < Chef::Provider::Package::Rubygems
 
-#   def load_current_resource
-#     @current_resource = Chef::Resource::Package::Rubygems.new(@new_resource.name)
-#      @current_resource = Chef::Provider::Package::Rubygems.new(@new_resource.name)
-#     @current_resource.package_name(@new_resource.package_name)
-#   end
-
-  def install_package(name, version)
+  def action_install
     options = @new_resource.options
-    cmd = "#{gem_binary_path} specific_install #{name} -l #{options[:repo]}"
+    cmd = "#{gem_binary_path} specific_install #{@new_resource.package_name} -l #{options[:repo]}"
     cmd << " -b #{options[:branch]}" if options[:branch]
+    logger.debug { "use #{cmd} to install"}
     shell_out!(cmd, :env=>nil)
   end
 
-#  def find_newest_remote_version(gem_dependency, *sources)
-#    logger.debug {"find_newest_remote_version not implement"}
-#    return []
-#  end
-
-#  def matching_installed_versions
-#    return []
-#  end
-
-
-#  def target_version_already_installed?   
-#    return false
-#  end
+  def candidate_version
+    logger.debug {"candidate_version not implement"}
+    return "0.0.0"
+  end
 end
