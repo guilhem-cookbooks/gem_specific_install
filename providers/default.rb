bb91cf92448cb8946f02eaf7406eab437d0839f1
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-09-02 15:01:15 +0200

    Add an LWRP to install a gem from git
    This is a workaround until https://github.com/rdp/specific_install/pull/14 is merged

---
+++ providers/default.rb
@@ -0,0 +1,39 @@
+use_inline_resources
+
+# Support whyrun
+def whyrun_supported?
+  true
+end
+
+action :install do
+  git path do
+    repository new_resource.repository
+    revision new_resource.revision if new_resource.revision
+    notifies :create, "ruby_block[install gem #{new_resource.gemname}]"
+  end
+
+  ruby_block "install gem #{new_resource.gemname}" do
+    block do
+      require 'rubygems/dependency_installer'
+      require 'rubygems/specification'
+      require 'rubygems/builder'
+
+      Dir.chdir(path)
+      gemspec = Gem::Specification.load(gemspec_file)
+      gem = Gem::Builder.new(gemspec).build
+      inst = Gem::DependencyInstaller.new
+      inst.install gem
+    end
+    action :nothing
+    only_if { ::File.exists?(gemspec_file) }
+  end
+end
+
+def path
+  ::File.join(Chef::Config[:file_cache_path], new_resource.gemname)
+end
+
+def gemspec_file
+  ::File.join(path, "#{new_resource.gemname}.gemspec")
+end
+
