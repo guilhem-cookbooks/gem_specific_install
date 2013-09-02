22b509d14fc0a466303f0573133981fa103df54a
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-09-02 15:02:37 +0200

    release v0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem.lettron@optiflows.com'
 license          'Apache 2.0'
 description      'Installs/Configures gem_specific_install'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 depends          'git'
 depends          'run_action_now'
