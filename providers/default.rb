1f3cf944b78255763d5d56bd36977a94721de5f9
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-09-10 15:27:04 +0200

    clear installation + remove notification for now.

--- providers/default.rb
+++ providers/default.rb
@@ -1,15 +1,14 @@
 use_inline_resources
 
-# Support whyrun
-def whyrun_supported?
-  true
-end
+include Chef::DSL::IncludeRecipe
 
 action :install do
+  include_recipe "gem_specific_install"
+
   git path do
     repository new_resource.repository
     revision new_resource.revision if new_resource.revision
-    notifies :create, "ruby_block[install gem #{new_resource.gemname}]"
+#    notifies :create, "ruby_block[install gem #{new_resource.gemname}]", :immediately
   end
 
   ruby_block "install gem #{new_resource.gemname}" do
@@ -18,14 +17,14 @@ action :install do
       require 'rubygems/specification'
       require 'rubygems/builder'
 
-      Dir.chdir(path)
-      gemspec = Gem::Specification.load(gemspec_file)
-      gem = Gem::Builder.new(gemspec).build
-      inst = Gem::DependencyInstaller.new
+      ::Dir.chdir(path)
+      gemspec = ::Gem::Specification.load(gemspec_file)
+      gem = ::Gem::Builder.new(gemspec).build
+      inst = ::Gem::DependencyInstaller.new
       inst.install gem
-      Gem.clearpaths
+      Gem.clear_paths
     end
-    action :nothing
+    action :create
     only_if { ::File.exists?(gemspec_file) }
   end
 end
